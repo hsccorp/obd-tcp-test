0236a87bf61b77ffb4363557b1d007d8769aeaf6
Arjun Roychowdhury <arjun@hsc.com>
2017-08-27 17:57:54 -0400

    init

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,46 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController,Platform } from 'ionic-angular';
+
+var wifiOBDReader;
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
+
+
 export class HomePage {
+  constructor(public navCtrl: NavController, public plt: Platform) {
+           plt.ready().then(() => {
+          console.log ("Platform ready, instantiating OBD");
+        
+            var OBDReader = require ('obd-bluetooth-tcp');
+            wifiOBDReader = new OBDReader();
 
-  constructor(public navCtrl: NavController) {
+            wifiOBDReader.on ('debug' , function (data) {console.log ("=>APP DEBUG:"+ data)});
+            wifiOBDReader.on ('error' , function (data) {console.log ("=>APP ERROR:"+ data)});
+            wifiOBDReader.setProtocol(0);
 
-  }
+            wifiOBDReader.autoconnect("TCP","192.168.1.103:5000");
 
-}
+            wifiOBDReader.on ('connected', function () {
+            console.log ("=>APP: Connected");
+            this.addPoller("temp");
+            this.addPoller("vss");
+            this.startPolling(2000); //Request  values every 2 second.
+
+            wifiOBDReader.on ('dataReceived', function (data) {
+              console.log ("=>APP: Received Data="+JSON.stringify(data));
+            })
+
+
+          }); // conneceted
+
+        }); // ready
+       } // constructor
+
+  } // class
+
+
+
