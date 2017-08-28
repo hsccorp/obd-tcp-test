0780b5815df38316a1525af09363c3f72a4703f5
Arjun Roychowdhury <arjun@hsc.com>
2017-08-28 17:00:18 -0400

    pretty graphs - why not

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,12 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { Ng2GoogleChartsModule } from 'ng2-google-charts';
+
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+
 
 @NgModule({
   declarations: [
@@ -14,6 +17,9 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
+    Ng2GoogleChartsModule,
+
+    
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,9 @@
-import { Component, NgZone } from '@angular/core';
+import { Component, NgZone, ViewChild } from '@angular/core';
 import { NavController, Platform } from 'ionic-angular';
+import { ChartReadyEvent } from 'ng2-google-charts';
 
+
+//declare var RadialGauge;
 
 var wifiOBDReader;
 
@@ -14,12 +17,89 @@ export class HomePage {
   wifiOBDReader: any;
   speed: string = "";
   temp: string = "";
+  
+  @ViewChild('speed') speedChart;
+  @ViewChild('temp') tempChart;
+
+   gaugeChartData:any =  {
+    chartType: 'Gauge',
+    dataTable: [
+      ['Label', 'Value'],
+      ['Speed', 0],
+
+
+    ],
+    options: {
+      animation: {easing: 'out'},
+      //width: 50%, height: 50%,
+      greenFrom: 1, greenTo: 60,
+      yellowFrom: 60, yellowTo: 110,
+      redFrom: 110, redTo:220,
+      minorTicks: 10,
+      min: 0, max: 220,
+      greenColor: '#019875',
+      yellowColor: '#F9BF3B',
+      redColor: '#EF4836',
+    }
+  };
+
+  tempChartData:any =  {
+    chartType: 'Gauge',
+    dataTable: [
+      ['Label', 'Value'],
+      ['Temp.', 0],
+
+
+    ],
+    options: {
+      animation: {easing: 'out'},
+      //width: 50%, height: 50%,
+      greenFrom: 1, greenTo: 60,
+      yellowFrom: 60, yellowTo: 110,
+      redFrom: 110, redTo:420,
+      minorTicks: 10,
+      min: -40, max: 420,
+      greenColor: '#019875',
+      yellowColor: '#F9BF3B',
+      redColor: '#EF4836',
+    }
+  };
+  
+  
+  change() {
+    this.changeSpeed();
+    this.changeTemp();
+  }
+
+  changeSpeed() {
+    let min = 0;
+    let max = 220;
+    let val = Math.floor(Math.random() * (max - min)) + min;
+    let data = this.speedChart.wrapper.getDataTable();
+    data.setValue(0,1,val);
+    this.speedChart.redraw();
+  }
+
+  changeTemp() {
+    let min = -40;
+    let max = 420;
+    let val = Math.floor(Math.random() * (max - min)) + min;
+    let data = this.tempChart.wrapper.getDataTable();
+    data.setValue(0,1,val);
+    console.log (JSON.stringify(data));
+    this.tempChart.redraw();
+  }
+
+  ready(event: ChartReadyEvent) {
+    
+  }
 
   constructor(public navCtrl: NavController, public plt: Platform, public zone: NgZone) {
 
     var OBDReader = require('obd-bluetooth-tcp');
     wifiOBDReader = new OBDReader();
     var instance = this;
+
 
     // set up handlers one time
     wifiOBDReader.on('debug', function (data) { console.log("=>APP DEBUG:" + data) });
@@ -30,10 +110,21 @@ export class HomePage {
       console.log("=>APP: Received Data=" + JSON.stringify(data));
 
       if (data.name && data.name == 'vss') {
-        setTimeout(() => { instance.speed = data.value + " km/hr"; }, 0);
+        let mph  = Math.round(data.value * 0.621371); // convert to mph
+        setTimeout(() => { 
+          instance.speed = mph+ " m/hr";  
+          let data = instance.speedChart.wrapper.getDataTable();
+          data.setValue(0,1,mph);
+          instance.speedChart.redraw(); }, 0);
       }
       if (data.name && data.name == 'temp') {
-        setTimeout(() => { instance.temp = data.value + " degrees"; }, 0);
+        let f = Math.round(data.value * 1.8 + 32); // convert to farenheit
+        setTimeout(() => { 
+          instance.temp = f + " degrees F"; 
+          let data = instance.tempChart.wrapper.getDataTable();
+          data.setValue(0,1,f);
+          instance.tempChart.redraw(); }, 0);
+      
       }
     })
 
@@ -50,6 +141,7 @@ export class HomePage {
     }); // conneceted
 
 
+
   } // constructor
 
   start() {
@@ -58,7 +150,6 @@ export class HomePage {
       console.log("Platform ready, instantiating OBD");
       wifiOBDReader.setProtocol(0);
       wifiOBDReader.autoconnect("TCP", this.host);
-
     }); // ready
   }
 
@@ -71,7 +162,10 @@ export class HomePage {
 
 
     });
+    
   }
+
+  
 
 } // class
 
